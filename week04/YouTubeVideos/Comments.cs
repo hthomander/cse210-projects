78639fc8cebd6c22039c88125c7293603bf87b49
hthomander <tho17074@byui.edu>
2025-01-29 21:33:47 -0700

    tryy

---
+++ week04/YouTubeVideos/Comments.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+public class Comment
+{
+    public string CommenterName {get; set;}
+    public string CommentText {get; set;}
+
+    public Comment(string commentName, string commentText)
+    {
+        CommenterName = CommenterName;
+        CommentText = commentText;
+    }
+}
--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+        Comment comment1 = new Comment("Alice", "Great View!");
+        Comment comment2 = new Comment("Bob", "Very Informative.");
+        Comment comment3 = new Comment("Charlie", "I learned a lot, thanks!");
+
+        Video video1 = new Video("Understanding C#", "Hazel Beth", 600);
+        video1.AddComment(comment1);
+        video1.AddComment(comment2);
+        video1.AddComment(comment3);
+
+        Console.WriteLine($"Title: {video1.Title}");
+        Console.WriteLine($"Author: {video1.Author}");
+        Console.WriteLine($"Length: {video1.Length} seconds");
+        Console.WriteLine($"Number of comments: {video1.GetNumberOfComments()}");
+        video1.DisplayCommments();
+
+        Comment comment4 = new Comment("Dave", "Amazing content!");
+        Video video2 = new Video("Mastering Algorithms", "Hazel Thomander", 720);
+        video2.AddComment(comment4);
+
+        Console.WriteLine("\nTitle: " + video2.Title);
+        Console.WriteLine("Author: " + video2.Author);
+        Console.WriteLine("Length: " + video2.Length + "seconds");
+        Console.WriteLine("Number of comments: " + video2.GetNumberOfComments());
+        video2.DisplayComments();
     }
 }
