19d5094b566151384fe9735a4d8323cc3d32479f
hthomander <tho17074@byui.edu>
2025-02-06 08:55:54 -0700

    save

---
+++ week05/Homework/Assignment.cs
@@ -0,0 +1,23 @@
+using System;
+
+public class Assignment 
+{
+    private string_studentName;
+    private string_topic;
+
+    public Assignment(string studentName, string topic)
+    {
+        _studentName = studentName;
+        _topic = topic;
+    }
+
+    public string GetSummary()
+    {
+        return $"{_studentName} -  {_topic}";
+    }
+
+    protected string GetStudentName()
+    {
+        return _studentName;
+    }
+}
--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+       MathAssignment mathAssignment = new MathAssignment("Jane Doe", "Fractions", "7.3", "Problems 8-19");
+       Console.WriteLine(mathAssignment.GetSummary());
+       Console.WriteLine(mathAssignment.GetHomeworkList());
+
+       Console.WriteLine();
+
+       WritingAssignment writingAssignment = new WritingAssignment("John Doe", "European History", "The Causes of World War II");
+       Console.WriteLine(writingAssignment.GetSummary());
+       Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
