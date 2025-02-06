6d8935ed9bba6a4c5f7d52d54ad16b3c81eeb016
hthomander <tho17074@byui.edu>
2025-02-06 10:47:45 -0700

    animation

--- week05/Mindfulness/Activity.cs
+++ week05/Mindfulness/Activity.cs
@@ -32,10 +32,21 @@ public abstract class Activity
 
     protected void Pause(int seconds)
     {
+       string[] spinChars = { "|", "/", "-", "\\"};
+       int spinIndex = 0; 
+
+
         for (int i = 0; i < seconds; i++)
         {
+            Console.Write(spinChars[spinIndex]);
+            Console.Write("\b");
+
+            spinIndex = (spinIndex + 1) % spinChars.Length;
             Thread.Sleep(1000);
-            Console.Write(".");
+
+            Console.Write($"{seconds - i} seconds remaining... \r");
+            Thread.Sleep(1000);
+            Console.Write("\b\b\b\b\b");
         }
         Console.WriteLine();
     }
